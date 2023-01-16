5959d42b140e97c276a11ea885d8a6ad7dc5d2a6
Kenichi Harada <s1511395@coins.tsukuba.ac.jp>
2023-01-16 19:23:03 +0900

    設問1に不要な記述を削除

--- answer1.py
+++ answer1.py
@@ -10,28 +10,24 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--file', required=True,
                         help='読み込む監視ログファイルのパス')
-    parser.add_argument('-N', '--numoftimeout',
-                        required=True, help='故障とみなす連続タイムアウト回数  設問2用')
-    parser.add_argument('-d', '--dst', required=False,
-                        help='レポートの出力先のパス 指定しない場合は規定のフォルダ下に出力する')
     return parser.parse_args()
 
 
 def main():
     args = get_args()
-    file = os.getcwd()+'log/samplelog.csv'  # args.file
-    N = 2  # args.numoftimeout
-    name_report = os.getcwd()+'/report/report1_.csv'  # +args.dst
-
+    file = args.file
+    file_report = os.getcwd()+'/report/report1_' + \
+        os.path.basename(file).split('.', 1)[0]+'.csv'
     # 監視対象の
-    num_watchlist = 4
+    num_watchlist = len(np.loadtxt(PATH_WATCHLIST, dtype='object').tolist())
     # 監視ファイルの読み込み
     # "-"が含まれているためresponse_msecはstringで読み込む
     dtype_log = {'datetime': 'string',
                  'ip': 'category', 'response_msec': 'string'}
     df = pd.read_csv(file, dtype=dtype_log)
-
     np_report = np.empty((0, 3), dtype='object')
+    columns_report = ['ip', 'datetime_start_timeout', 'datetime_end_timeout']
+    print(columns_report)
     for idx_ip in range(num_watchlist):
         ip_name = df.iat[idx_ip, 1]
         df_thisip = df.loc[idx_ip::num_watchlist]
@@ -58,9 +54,8 @@ def main():
             print(ip_name + ',' + datetime_start_timeout +
                   ','+datetime_end_timeout)
 
-    columns_report = ['ip', 'datetime_start_timeout', 'datetime_end_timeout']
     df_report = pd.DataFrame(data=np_report, columns=columns_report)
-    df_report.to_csv(name_report, index=False)
+    df_report.to_csv(file_report, index=False)
 
 
 if __name__ == '__main__':
